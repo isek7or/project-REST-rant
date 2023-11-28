419759b60f67cc94622d848991f38a133b5fe140
Carlos Sosa <isek7or@gmail.com>
2023-11-28 15:27:42 -0800

    Adding and rendering a new view

--- views/default.jsx
+++ views/default.jsx
@@ -5,7 +5,7 @@ function Def(html) {
         <html>
             <head>
                 <title>Title</title>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous" />
                 <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
