0e287a60d78f6d3d9fb606985350ac3bc81eb44d
Carlos Sosa <isek7or@gmail.com>
2023-12-05 21:11:23 -0800

    Created and Used a Seeder File

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, 
+  useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
