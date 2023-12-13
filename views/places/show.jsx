e4be14f49f527f91097b1056c198d7c40d91864e
Carlos Sosa <isek7or@gmail.com>
2023-12-12 22:13:11 -0800

    Populated the Comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show(data) {
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
   return (
     <Def>
       <main>
@@ -13,7 +32,7 @@ function show(data) {
           <div className="col-sm-6">
             <h1>{data.place.name}</h1>
             <h2>Rating</h2>
-            <p>Not Rated</p>
+            <h3>Not Rated</h3>
             <br />
             <h2>Description</h2>
             <h3>{data.place.showEstablished()}</h3>
@@ -31,7 +50,7 @@ function show(data) {
         </div>
         <hr />
         <h2>Comments</h2>
-        <p>No Comments yet!</p>
+        {comments}
       </main>
     </Def>
   )
