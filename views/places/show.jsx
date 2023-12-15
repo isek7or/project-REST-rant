4f9853b6ee795af2173a29e54fe6debd50161852
Carlos Sosa <isek7or@gmail.com>
2023-12-14 22:40:35 -0800

    Completed REST-Rant Part 11 plus Bonus

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,25 @@ function show(data) {
       No comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i < averageRating; i++) {
+      stars += '⭐️'
+    }
+    rating = (
+      <h3>
+        {stars} stars
+      </h3>
+    )
     comments = data.place.comments.map(c => {
       return (
         <div className="border col-sm-4">
@@ -17,6 +35,9 @@ function show(data) {
             <stong>- {c.author}</stong>
           </h3>
           <h4>Rating: {c.stars}</h4>
+          <form method="POST" action={`/places/${data.place.id}/comment/${c.id}?_method=DELETE`}>
+            <input type="submit" className="btn btn-danger" value="Delete Comment" />
+          </form>
         </div>
       )
     })
@@ -32,16 +53,16 @@ function show(data) {
           <div className="col-sm-6">
             <h1>{data.place.name}</h1>
             <h2>Rating</h2>
-            <h3>Not Rated</h3>
+            {rating}
             <br />
             <h2>Description</h2>
             <h3>{data.place.showEstablished()}</h3>
             <h4>Serving {data.place.cuisines}</h4>
             <br />
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+            <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
               Edit
             </a>
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+            <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
               <button type="submit" className="btn btn-danger">
                 Delete
               </button>
@@ -50,7 +71,7 @@ function show(data) {
         </div>
         <hr />
         <h2>Comments</h2>
-        <div className="row">
+        <div className="row justify-content-md-center">
           {comments}
         </div>
         <hr />
